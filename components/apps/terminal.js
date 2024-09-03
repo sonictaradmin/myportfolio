f15154ffb94ea42744d0b60059e4ad07c29bb3f3
sonictaradmin <tarnetsonic@gmail.com>
2024-09-03 11:24:05 +0530

    Update terminal.js

--- components/apps/terminal.js
+++ components/apps/terminal.js
@@ -281,7 +281,7 @@ export class Terminal extends Component {
                 break;
             case "about-vivek":
                 if (words[0] === "." || words.length === 0) {
-                    this.props.openApp("about-vivek");
+                    this.props.openApp("about-tamilselvam");
                 } else {
                     result = "Command '" + main + "' not found, or not yet implemented.<br>Available Commands: [ cd, ls, pwd, echo, clear, exit, mkdir, code, spotify, chrome, about-tamilselvam, todoist, trash, settings, sendmsg ]";
                 }
